a3a4a47a9be993daf52d4ea2311256cba3f5a9ed
Gwen <56498304+gwen0902@users.noreply.github.com>
2023-10-22 14:53:41 +0700

    Add files via upload

---
+++ Dictionary/js/script.js
@@ -0,0 +1,30 @@
+
+    const header = document.querySelector('header');
+
+        window.addEventListener('scroll', () => {
+            if (window.scrollY >= 80) {
+                header.classList.add('active_header');
+            } else {
+                header.classList.remove('active_header');
+            }
+        })
+        
+
+        function myFunction() {
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("myInput");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("myTable");
+            tr = table.getElementsByTagName("tr");
+            for (i = 0; i < tr.length; i++) {
+              td = tr[i].getElementsByTagName("td")[0];
+              if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                  tr[i].style.display = "";
+                } else {
+                  tr[i].style.display = "none";
+                }
+              }       
+            }
+          }
